ad805bc39ecbb6b687fbf972be01a59bc99a907b
20210298 <144835269+20210298@users.noreply.github.com>
2024-09-09 15:20:48 +0900

    20240909

---
+++ FileName.cpp
@@ -0,0 +1,71 @@
+#include <windows.h> 
+#include <stdio.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszArg, int nCmdShow)
+{
+	HWND hWnd;
+	MSG msg;
+	WNDCLASS WndClass;
+	char szAppName[] = "Hello";
+	WndClass.style = NULL;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.lpszMenuName = NULL;
+	WndClass.lpszClassName = szAppName;
+
+	if (!RegisterClass(&WndClass)) return NULL;
+
+	hWnd = CreateWindow(
+		szAppName,
+		szAppName,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT mesg, WPARAM wParam, LPARAM lParam)
+{
+	switch (mesg)
+	{
+	case WM_MOUSEMOVE:
+		int x, y;
+		x = LOWORD(lParam);
+		y = HIWORD(lParam);
+		char szPos[256];
+		sprintf(szPos, "%04d %04d", x, y);
+
+		HDC hdc;
+		hdc = GetDC(hWnd);
+		TextOut(hdc, 0, 0, szPos, strlen(szPos));
+		ReleaseDC(hWnd, hdc);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return FALSE;
+	}
+	return DefWindowProc(hWnd, mesg, wParam, lParam);
+}
